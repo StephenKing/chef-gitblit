6fa6c2233065ea814efcf94865a83673af91c2ed
Christian Trabold <christian.trabold@dkd.de>
2012-08-09 18:02:06 +0700

    Initial version

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "TYPO3 Association"
+maintainer_email "steffen.gebert@typo3.org"
+license          "MIT"
+description      "Installs/Configures gitblit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "application"
+depends "application_java"
